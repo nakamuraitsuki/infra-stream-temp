d3824fd3858a7c287e923950bf03856d7d3b5bb7
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-08 17:17:38 +0900

    feat: コンストラクタを定義

--- backend/internal/domain/video/entity.go
+++ backend/internal/domain/video/entity.go
@@ -21,3 +21,29 @@ type Video struct {
 	visibility    value.Visibility
 	createdAt     time.Time
 }
+
+func NewVideo(
+	id uuid.UUID,
+	ownerID uuid.UUID,
+	sourceKey string,
+	streamKey string,
+	status value.Status,
+	title string,
+	description string,
+	tags []value.Tag,
+	visibility value.Visibility,
+	createdAt time.Time,
+) *Video {
+	return &Video{
+		id:          id,
+		ownerID:     ownerID,
+		sourceKey:   sourceKey,
+		streamKey:   streamKey,
+		status:      status,
+		title:       title,
+		description: description,
+		tags:        tags,
+		visibility:  visibility,
+		createdAt:   createdAt,
+	}
+}
