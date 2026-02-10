fdc819673335ceba080fd396c6ce7aea925857e3
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-10 14:22:54 +0900

    feat: ダミーログイン機構をルーターに仕込む

--- backend/internal/interface/http/user/routes.go
+++ backend/internal/interface/http/user/routes.go
@@ -1,12 +1,17 @@
 package user
 
-import "github.com/labstack/echo/v4"
+import (
+	"example.com/m/internal/interface/http/middleware"
+	"github.com/labstack/echo/v4"
+)
 
 func RegisterRoutes(e *echo.Echo, h *Handler) {
 	e.POST("/users", h.Register)
+	e.POST("/users/login", h.DummyLogin) // 認証不要な仮ログインエンドポイント
+	e.POST("/users/logout", h.Logout)
 
 	auth := e.Group("/users")
-	// TODO: Add authentication middleware here
+	auth.Use(middleware.DummyAuthMiddleware)
 	
 	auth.GET("/me", h.GetMe)
 	auth.PUT("/me/profile", h.UpdateProfile)
