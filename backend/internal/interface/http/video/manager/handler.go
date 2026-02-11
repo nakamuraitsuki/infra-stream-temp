409a1684f9c6b736cdf99d4791fc2d2f9d4d1729
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-11 16:18:37 +0900

    fix: 不要な引数を削除

--- backend/internal/interface/http/video/manager/handler.go
+++ backend/internal/interface/http/video/manager/handler.go
@@ -2,14 +2,13 @@ package manager
 
 import (
 	"example.com/m/internal/usecase/video/manage"
-	"example.com/m/internal/usecase/video/view"
 )
 
 type VideoManagementHandler struct {
 	manageUsecase manage.VideoManagementUseCaseInterface
 }
 
-func NewVideoManagementHandler(manageUsecase *manage.VideoManagementUseCase, viewUsecase *view.VideoViewingUseCase) *VideoManagementHandler {
+func NewVideoManagementHandler(manageUsecase manage.VideoManagementUseCaseInterface) *VideoManagementHandler {
 	return &VideoManagementHandler{
 		manageUsecase: manageUsecase,
 	}
