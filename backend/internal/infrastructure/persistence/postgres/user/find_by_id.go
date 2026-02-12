22d2e88b3fe5419b086cf5784885dd27e493daef
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-12 11:11:34 +0900

    feat: methodの実装

--- backend/internal/infrastructure/persistence/postgres/user/find_by_id.go
+++ backend/internal/infrastructure/persistence/postgres/user/find_by_id.go
@@ -2,12 +2,34 @@ package user
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 
 	user_domain "example.com/m/internal/domain/user"
+	"example.com/m/internal/infrastructure/persistence/postgres"
 	"github.com/google/uuid"
 )
 
 func (r *userRepository) FindByID(ctx context.Context, id uuid.UUID) (*user_domain.User, error) {
-	// TODO: implement
-	return nil, nil
+	db := postgres.GetExt(ctx, r.db)
+
+	const query = `
+SELECT
+	id, name, bio, icon_key, role, created_at
+FROM
+	users
+WHERE id = $1
+`
+
+	var dao userDAO
+	err := db.GetContext(ctx, &dao, query, id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("failed to find user by id: %w", err)
+		}
+		return nil, fmt.Errorf("failed to find user by id: %w", err)
+	}
+
+	return dao.toEntity()
 }
--- backend/internal/infrastructure/persistence/postgres/user/save.go
+++ backend/internal/infrastructure/persistence/postgres/user/save.go
@@ -2,11 +2,35 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	user_domain "example.com/m/internal/domain/user"
+	"example.com/m/internal/infrastructure/persistence/postgres"
+	"github.com/jmoiron/sqlx"
 )
 
 func (r *userRepository) Save(ctx context.Context, user *user_domain.User) error {
-	// TODO: implement
+	db := postgres.GetExt(ctx, r.db)
+
+	model := fromEntity(user)
+
+	const query = `
+INSERT INTO users (
+	id, name, bio, icon_key, role, created_at
+) VALUES (
+	:id, :name, :bio, :icon_key, :role, :created_at
+)
+ON CONFLICT (id) DO UPDATE SET
+	name = EXCLUDED.name,
+	bio = EXCLUDED.bio,
+	icon_key = EXCLUDED.icon_key,
+	role = EXCLUDED.role
+`
+
+	_, err := sqlx.NamedExecContext(ctx, db, query, model)
+	if err != nil {
+		return fmt.Errorf("failed to save user: %w", err)
+	}
+
 	return nil
 }
