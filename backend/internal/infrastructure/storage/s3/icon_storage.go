318054c63bb30567e6147529141e9a31803801ad
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-12 21:54:34 +0900

    feat: IconStorage骨子

--- backend/internal/infrastructure/storage/s3/icon_storage.go
+++ backend/internal/infrastructure/storage/s3/icon_storage.go
@@ -1,1 +1,30 @@
 package s3
+
+import (
+	"context"
+
+	"example.com/m/internal/domain/user"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type iconStorage struct {
+	client *s3.Client
+	bucketName string
+}
+
+func NewIconStorage(client *s3.Client, bucketName string) user.IconStorage {
+	return &iconStorage{
+		client: client,
+		bucketName: bucketName,
+	}
+}
+
+func (s *iconStorage) Upload(ctx context.Context, key string, data []byte) error {
+	// implement
+	return nil
+}
+
+func (s *iconStorage) Delete(ctx context.Context, key string) error {
+	// implement
+	return nil
+}
