43a3b4fcdf5ae538ef20bb0b8ff8e7a3a2074578
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-12 22:29:48 +0900

    feat: icon storage

--- backend/internal/infrastructure/storage/s3/icon_storage.go
+++ backend/internal/infrastructure/storage/s3/icon_storage.go
@@ -1,30 +1,47 @@
 package s3
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 
 	"example.com/m/internal/domain/user"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type iconStorage struct {
-	client *s3.Client
+	client     *s3.Client
 	bucketName string
 }
 
 func NewIconStorage(client *s3.Client, bucketName string) user.IconStorage {
 	return &iconStorage{
-		client: client,
+		client:     client,
 		bucketName: bucketName,
 	}
 }
 
 func (s *iconStorage) Upload(ctx context.Context, key string, data []byte) error {
-	// implement
+	_, err := s.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(s.bucketName),
+		Key:         aws.String(key),
+		Body:        bytes.NewReader(data),
+		ContentType: aws.String("image/png"),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload icon to s3: %w", err)
+	}
 	return nil
 }
 
 func (s *iconStorage) Delete(ctx context.Context, key string) error {
-	// implement
+	_, err := s.client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(s.bucketName),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete icon from s3: %w", err)
+	}
 	return nil
 }
