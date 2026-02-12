18c949e45c8a2a33de0c0cf21ddae7e205d40735
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-13 00:06:52 +0900

    feat: factory

--- backend/internal/domain/video/storage.go
+++ backend/internal/domain/video/storage.go
@@ -6,7 +6,10 @@ import (
 )
 
 type Storage interface {
+	// 元動画の保存（外部アップロード）
 	SaveSource(ctx context.Context, key string, data io.Reader) error
+ 
+	// 変換後動画の保存（Transcodeプロセスからのアップロード）
 	SaveStream(ctx context.Context, key string, data io.Reader) error
 	GetStream(ctx context.Context, key string) (io.ReadSeeker, error)
 	Delete(ctx context.Context, key string) error
--- backend/internal/infrastructure/storage/s3/video_storage.go
+++ backend/internal/infrastructure/storage/s3/video_storage.go
@@ -1,1 +1,18 @@
 package s3
+
+import (
+	"example.com/m/internal/domain/video"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type videoStorage struct {
+	client     *s3.Client
+	bucketName string
+}
+
+func NewVideoStorage(client *s3.Client, bucketName string) video.Storage {
+	return &videoStorage{
+		client:     client,
+		bucketName: bucketName,
+	}
+}
