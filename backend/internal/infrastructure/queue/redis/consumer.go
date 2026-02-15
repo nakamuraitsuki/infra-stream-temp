faa5bfab05898d18a5b44c673361a5404c1c30bd
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-15 22:37:55 +0900

    feat: Redisコンシューマのワーカーとウォッチャーの実装を追加

--- backend/internal/infrastructure/queue/redis/consumer.go
+++ backend/internal/infrastructure/queue/redis/consumer.go
@@ -10,6 +10,12 @@ import (
 
 	"example.com/m/internal/usecase/job"
 	"github.com/go-redis/redis/v8"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	TRANSCODE_WORKER_POOL_SIZE = 4
+	JOB_CHANNEL_BUFFER_SIZE    = TRANSCODE_WORKER_POOL_SIZE * 2 // ワーカーが待機する分のバッファを確保
 )
 
 type consumer struct {
@@ -37,11 +43,29 @@ func (c *consumer) Register(jobType string, h job.Handler) {
 
 func (c *consumer) Start(ctx context.Context) error {
 	log.Printf("Starting Redis Consumer [key: %s]", c.key)
+	jobCh := make(chan jobMessage, JOB_CHANNEL_BUFFER_SIZE)
+	eg, gCtx := errgroup.WithContext(ctx)
 
+	eg.Go(func() error {
+		return c.workerPool(gCtx, TRANSCODE_WORKER_POOL_SIZE, jobCh)
+	})
+
+	eg.Go(func() error {
+		defer close(jobCh) // watcherが終わったらworkerに終了を通知
+		return c.watcher(gCtx, jobCh)
+	})
+
+	return eg.Wait()
+}
+
+func (c *consumer) watcher(
+	ctx context.Context,
+	jobCh chan<- jobMessage,
+) error {
 	for {
 		select {
 		case <-ctx.Done():
-			return ctx.Err()
+			return nil
 		default:
 			res, err := c.client.Universal.BRPop(ctx, 5*time.Second, c.key).Result()
 			if err != nil {
@@ -58,37 +82,72 @@ func (c *consumer) Start(ctx context.Context) error {
 				continue
 			}
 
-			// helper関数に処理を任せる
-			// TODO: ゴルーチンリークまっしぐらなので、worker pool化するなどの対策が必要
-			go c.dispatch(ctx, []byte(res[1]))
+			var msg jobMessage
+			if err := json.Unmarshal([]byte(res[1]), &msg); err != nil {
+				log.Printf("Failed to unmarshal job message: %v", err)
+				continue
+			}
+
+			select {
+			case jobCh <- msg:
+			case <-ctx.Done():
+				return nil
+			}
 		}
 	}
 }
 
-func (c *consumer) dispatch(ctx context.Context, data []byte) {
-	var msg jobMessage
-	if err := json.Unmarshal(data, &msg); err != nil {
-		log.Printf("Failed to unmarshal job message: %v", err)
-		return
+func (c *consumer) workerPool(
+	ctx context.Context,
+	numWorkers int,
+	jobCh <-chan jobMessage,
+) error {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for range numWorkers {
+		eg.Go(func() error {
+			for {
+				select {
+				// -- contextキャンセルの検知 --
+				case <-ctx.Done():
+					return nil
+
+				// -- ジョブの処理 --
+				case msgByte, ok := <-jobCh:
+					if !ok {
+						return nil // channelが閉じられた場合
+					}
+					if err := c.dispatch(ctx, msgByte); err != nil {
+						return err
+					}
+				}
+			}
+		})
 	}
 
+	return eg.Wait()
+}
+
+func (c *consumer) dispatch(ctx context.Context, jobMsg jobMessage) error {
 	c.mu.RLock()
-	handler, ok := c.handlers[msg.Meta.Type]
+	handler, ok := c.handlers[jobMsg.Meta.Type]
 	c.mu.RUnlock()
 
 	if !ok {
-		log.Printf("No handler registered for job type: %s", msg.Meta.Type)
-		return
+		log.Printf("No handler registered for job type: %s", jobMsg.Meta.Type)
+		// NOTE: リトライ処理を図るので、Consumer群に伝播させない
+		return nil
 	}
 
-	if err := handler.Handle(ctx, msg.Meta, msg.Payload); err != nil {
-		log.Printf("Error handling job [type: %s, id: %s]: %v", msg.Meta.Type, msg.Meta.ID, err)
+	if err := handler.Handle(ctx, jobMsg.Meta, jobMsg.Payload); err != nil {
+		log.Printf("Error handling job [type: %s, id: %s]: %v", jobMsg.Meta.Type, jobMsg.Meta.ID, err)
 		// NOTE: retry
-		if msg.Meta.Attempt < msg.Meta.MaxRetry {
-			msg.Meta.Attempt++
-			if err := c.queue.Enqueue(ctx, msg.Meta, msg.Payload); err != nil {
-				log.Printf("Failed to re-enqueue job [type: %s, id: %s]: %v", msg.Meta.Type, msg.Meta.ID, err)
+		if jobMsg.Meta.Attempt < jobMsg.Meta.MaxRetry {
+			jobMsg.Meta.Attempt++
+			if err := c.queue.Enqueue(ctx, jobMsg.Meta, jobMsg.Payload); err != nil {
+				log.Printf("Failed to re-enqueue job [type: %s, id: %s]: %v", jobMsg.Meta.Type, jobMsg.Meta.ID, err)
 			}
 		}
 	}
+	return nil
 }
