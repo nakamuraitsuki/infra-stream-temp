b33af3d5d5110320c783117bdcdd026017a0114b
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-16 14:32:37 +0900

    feat: httpのmain.goに初期化ロジックを記載

--- backend/cmd/http/main.go
+++ backend/cmd/http/main.go
@@ -1,6 +1,12 @@
 package main
 
-// TODO: implement main function
+import "log"
+
 func main() {
-	// Application entry point
+	e, err := InitializeHTTPServer()
+	if err != nil {
+		log.Fatalf("サーバーの初期化に失敗しました: %v", err)
+	}
+	
+	e.Logger.Fatal(e.Start(":8080"))
 }
