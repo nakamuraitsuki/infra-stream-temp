7049ec638f55b5de0bc1dbe0de091472f11a4b6c
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-18 12:16:53 +0900

    feat: 動画解析実装

---
+++ frontend/src/gateways/video/video.service.impl.ts
@@ -0,0 +1,77 @@
+import Hls from "hls.js";
+import type { IVideoAnalyzer, VideoMetadata } from "../../domain/video/video.service";
+
+// cf. https://github.com/video-dev/hls.js
+export class HlsVideoAnalyzer implements IVideoAnalyzer {
+  async analyzeFromUrl(url: string): Promise<VideoMetadata> {
+    return new Promise((resolve, reject) => {
+
+      // NOTE: メモリやリスナーを独立させるため、毎回新しいHlsインスタンスを作成する
+      const hls = new Hls();
+
+      hls.loadSource(url);
+
+      // 成功イベントリスナー
+      hls.on(Hls.Events.MANIFEST_PARSED, (_event, data) => {
+        const levels = data.levels;
+        if (levels.length === 0) {
+          hls.destroy();
+        }
+
+        const qualities = levels
+          .map((l) => `${l.height}p`)
+          .filter((q, index, self) => self.indexOf(q) === index); // 重複削除
+
+        const best = levels[0]
+
+        resolve({
+          duration: 0, // HLSは全体のdurationがわからないことが多い
+          width: best.width,
+          height: best.height,
+          bitrate: best.bitrate,
+          hasAudio: true, // 一旦true固定
+          qualities,
+        });
+
+        hls.destroy();
+      });
+
+      // エラーイベントリスナー
+      hls.on(Hls.Events.ERROR, (_event, data) => {
+        if (data.fatal) {
+          hls.destroy();
+          reject(new Error(`HLS analysis failed: ${data.type} - ${data.details}`));
+        }
+      });
+    });
+  }
+
+  async analyzeFromFile(file: File): Promise<VideoMetadata> {
+    return new Promise((resolve, reject) => {
+      // NOTE: ローカルファイルは、一度ブラウザに読ませる
+      const video = document.createElement("video");
+      video.preload = "metadata";
+
+      const objectUrl = URL.createObjectURL(file);
+      video.src = objectUrl;
+
+      video.onloadedmetadata = () => {
+        URL.revokeObjectURL(objectUrl); // メモリ解放
+
+        resolve({
+          duration: video.duration,
+          width: video.videoWidth,
+          height: video.videoHeight,
+          bitrate: 0, // local からはビットレートはわからないため0固定
+          hasAudio: true, // 一旦true固定
+          qualities: [`${video.videoHeight}p`], 
+        })
+      }
+
+      video.onerror = (e) => {
+        URL.revokeObjectURL(objectUrl); // メモリ解放
+        reject(new Error(`Video analysis failed: ${e}`));
+      }
+    })
+  }
+}
