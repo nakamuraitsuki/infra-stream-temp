84ef9f0cb60bd67cc425eb909197747226726ab0
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-22 16:43:35 +0900

    chore: use-queryの基本設定

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,23 @@
 import { RouterProvider } from "react-router/dom"
 import { router } from "./router"
 import { AppProviders } from "./providers"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 export default function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <AppProviders>
       <RouterProvider router={router} />
     </AppProviders>
+    </QueryClientProvider>
   )
 }
